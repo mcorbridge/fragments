af19ed28dea1ee1fb4cae16f4a8a0fc687f96dd1
michael corbridge <mcorbridge@gmail.com>
2014-12-01 15:31:57 -0500

    adding rest of the app

---
+++ app/src/main/java/com/mcorbridge/testfragments/PM_Fragment.java
@@ -0,0 +1,23 @@
+package com.mcorbridge.testfragments;
+
+/**
+ * Created by Mike on 11/29/2014.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PM_Fragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        /**
+         * Inflate the layout for this fragment
+         */
+        return inflater.inflate(
+                R.layout.pm_fragment, container, false);
+    }
+}
